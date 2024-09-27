6a4178ecfb175836749cb4ef33e77553b5066f5a
Christopher Roelle <christopher.roelle@gmail.com>
2024-09-27 17:37:23 -0500

    Added Base Interaction System
    Added:
    + Wait Stars
    + Coroutine for enabling stars on interactive object
    + Added Registering Triggers to Interact

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -9,10 +10,19 @@ public class Player : MonoBehaviour
     Vector2 mousePosition = Vector2.zero;
 
     [SerializeField]
+    float waitStarDelayInSeconds = 0.5f;
+
+    [SerializeField]
     Camera mainCamera;
 
     [SerializeField]
     GameObject cursorObject;
+
+    bool mouseCursorFound = false;
+    Collider2D cursorColider;
+    List<SpriteRenderer> waitStars;
+
+    private Coroutine starCoroutine;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +33,12 @@ public class Player : MonoBehaviour
         }
 
         InitializeMouse();
+
+        //Get Wait Stars if they exist
+        if(mouseCursorFound)
+        {
+            GetWaitStars();
+        }
     }
 
     // Update is called once per frame
@@ -41,9 +57,48 @@ public class Player : MonoBehaviour
         }
         else
         {
-            //Hide the cursor
-            Cursor.visible = false;
+            //Get the circle collider
+            try
+            {
+                cursorColider = cursorObject.GetComponent<Collider2D>();
+
+                //Hide the cursor
+                Cursor.visible = false;
+                
+                mouseCursorFound = true;
+            }
+            catch
+            {
+                Debug.Log("Cursor - No collider found!");
+                Cursor.visible = true;
+            }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"{collision.gameObject.tag}");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(starCoroutine == null)
+        {
+            starCoroutine = StartCoroutine(EnableStarsGradually());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (starCoroutine != null)
+        {
+            //Reset the coroutine
+            StopCoroutine(starCoroutine);
+            starCoroutine = null;
+        }
+
+        //Turn off any stars that were active
+        DisableAllWaitStars();
     }
 
     void PollMousePosition()
@@ -64,6 +119,62 @@ public class Player : MonoBehaviour
 
             //Move cursor object
             cursorObject.transform.position = clampedWorldPosition;
+        }
+    }
+
+    private void GetWaitStars()
+    {
+        waitStars = new List<SpriteRenderer>();
+
+        foreach(Transform child in this.transform)
+        {
+            SpriteRenderer childRenderer = child.GetComponent<SpriteRenderer>();
+            waitStars.Add(childRenderer);
+        }
+
+        //Set them to inactive if there are stars
+        DisableAllWaitStars();
+    }
+
+    private IEnumerator EnableStarsGradually()
+    {
+        if (waitStars.Count > 0)
+        {
+            foreach (var waitStar in waitStars)
+            {
+                //enable current star
+                waitStar.enabled = true;
+
+                //Wait 'x' seconds before next
+                yield return new WaitForSeconds(waitStarDelayInSeconds);
+            }
+        }
+
+        //If all stars have loaded, lets load the new scene from the trigger
+        LoadLevel("");
+    }
+
+    private void DisableAllWaitStars()
+    {
+        if (waitStars.Count > 0)
+        {
+            foreach (var waitStar in waitStars)
+            {
+                waitStar.enabled = false;
+            }
+        }
+    }
+
+    private void LoadLevel(string level)
+    {
+        if (!string.IsNullOrEmpty(level))
+        {
+            DisableAllWaitStars();
+            SceneManager.LoadScene(level);
+        }
+        else
+        {
+            Debug.Log("Level - No Level to load!");
         }
     }
 }
